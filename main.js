837aac5a00f48a92eb9d593f412ff627ff065bbb
Mary Alice Orr <m.a.orr17@gmail.com>
2018-03-22 21:03:49 -0500

    wip commit

---
+++ main.js
@@ -0,0 +1,43 @@
+console.log("hello");
+
+const destinationCards = [
+    {
+        title: "Endor",
+        id: 1,
+        image: "/images/endor.jpg",
+        description: "small forested moon populated by Ewoks"
+    },
+    {
+        title: "Moab, UT",
+        id: 2,
+        image: "/images/endor.jpg",
+        description: "lkasdjflksajfdklads"
+    },
+    {
+        title: "Grand Tetons, WY",
+        id: 3,
+        image: "/images/endor.jpg",
+        description: "ljsadflkjasdlfkjas"
+    },
+    {
+        title: "NYC, NY",
+        id: 4,
+        image: "/images/endor.jpg",
+        description: "lkasjdflkjaslkfj"
+    },
+    {
+        title: "Napa, CA",
+        id: 5,
+        image: "/images/endor.jpg",
+        description: "kljasdfljaslfkjass"
+    },
+    {
+        title: "Denver, CO",
+        id: 6,
+        image: "/images/endor.jpg",
+        description: "bllakjdflkajsfklas"
+    },
+];
+
+
+
