cf26afb2e3fbf8ebc41ebf20db21ddd1602017f1
Mary Alice Orr <m.a.orr17@gmail.com>
2018-03-28 22:46:04 -0500

    Added destination cards and styled

--- main.js
+++ main.js
@@ -2,42 +2,82 @@ console.log("hello");
 
 const destinationCards = [
     {
-        title: "Endor",
+        title: "Maui, Hawaii",
         id: 1,
-        image: "/images/endor.jpg",
-        description: "small forested moon populated by Ewoks"
+        image: "images/maui.jpg",
+        description: "Maui is an island in the Central Pacific, part of the Hawaiian archipelago. Sprawling Haleakala National Park encompasses the island’s highest peak, volcanic Haleakala, as well as the pools and waterfalls of Ohe’o Gulch, accessed via scenic, winding Hana Highway. The island's 30 miles of beaches include golden-crescent Kapalua, sheltered from strong currents by lava-rock promontories."
     },
     {
         title: "Moab, UT",
         id: 2,
-        image: "/images/endor.jpg",
-        description: "lkasdjflksajfdklads"
+        image: "images/moab.jpg",
+        description: "Moab is a city in eastern Utah. It’s a gateway to massive red rock formations in Arches National Park. Southwest, Canyonlands National Park features mesas and buttes carved by the Green and Colorado rivers, plus Native American rock art. Dinosaur tracks can be found at sites like Bull Canyon Overlook and Copper Ridge. In the city, collections at the Museum of Moab include dinosaur bones and archaeological artifacts."
     },
     {
         title: "Grand Tetons, WY",
         id: 3,
-        image: "/images/endor.jpg",
-        description: "ljsadflkjasdlfkjas"
+        image: "images/tetons.jpg",
+        description: "Grand Teton National Park is in the northwest of the U.S state of Wyoming. It encompasses the Teton mountain range, the 4,000-meter Grand Teton peak, and the valley known as Jackson Hole. It’s a popular destination in summer for mountaineering, hiking, backcountry camping and fishing, linked to nearby Yellowstone National Park by the John D. Rockefeller, Jr. Memorial Parkway."
     },
     {
         title: "NYC, NY",
         id: 4,
-        image: "/images/endor.jpg",
-        description: "lkasjdflkjaslkfj"
+        image: "images/nyc.jpg",
+        description: "New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean. At its core is Manhattan, a densely populated borough that’s among the world’s major commercial, financial and cultural centers. Its iconic sites include skyscrapers such as the Empire State Building and sprawling Central Park. Broadway theater is staged in neon-lit Times Square."
     },
     {
         title: "Napa, CA",
         id: 5,
-        image: "/images/endor.jpg",
-        description: "kljasdfljaslfkjass"
+        image: "images/napa.jpg",
+        description: "Napa is the seat of Napa County, California, in the heart of the Napa Valley wine region. Its downtown is known for late-1800s and early-1900s architecture, a riverfront promenade with shops and restaurants, and an arts scene anchored by the Napa Valley Opera House. The Oxbow Public Market houses a farmer's market and cheese shop, and the Napa Valley Wine Train visits area wineries via vintage Pullman cars."
     },
     {
         title: "Denver, CO",
         id: 6,
-        image: "/images/endor.jpg",
-        description: "bllakjdflkajsfklas"
+        image: "images/denver.jpg",
+        description: "Denver, the capital of Colorado, is an American metropolis dating to the Old West era. Larimer Square, the city’s oldest block, features landmark 19th-century buildings. Museums include the Denver Art Museum, an ultramodern complex known for its collection of indigenous works, and the mansion of famed Titanic survivor Molly Brown. Denver is also a jumping-off point for ski resorts in the nearby Rocky Mountains."
     },
 ];
 
+const writeToDom = (string, id) => {
+    const writeTo =  document.getElementById(id)
+    writeTo.innerHTML = string;
+};
 
+const destinationCardBuilder = (destinationCardArray) => {
+    let destinationString = "";
+    destinationCardArray.forEach((destination) => {
+        destinationString += `<div class="destination-card">`;
+        destinationString +=    `<h2>${destination.title}</h2>`;
+        destinationString +=    `<img src="${destination.image}" alt="">`;
+        destinationString +=    `<p>${destination.description}</p>`;
+        destinationString += `  <textarea id="${destination.id}" rows="15" cols="50"></textarea>`;
+        destinationString += `  <button type="button">Submit</button>`;
+        destinationString += `</div>`;
+    });
+    writeToDom(destinationString, "destination-holder");
+}
 
+destinationCardBuilder(destinationCards);
+
+const diaryEntryCardBuilder = (diaryCardArray) => {
+    let diaryString = "";
+    diaryCardArray.forEach((diary) => {
+    diaryString += `<div class="diary-entry-holder">`;
+    diaryString +=     `<h2>${diary.title}</h2>`;
+    diaryString +=     `<p>${"kajkjhakjhfkhaf"}</p>`;
+    diaryString += `</div>`;
+    })
+    writeToDom(diaryString, "diary-holder")
+}
+
+diaryEntryCardBuilder(destinationCards);
+
+// const textBox = document.getElementsById(destinationCards.id);
+
+// const buttonClick = () => {
+//     buttonClick.addEventListener("click", (e) => {
+//         const textAreaInput = textBox.value();
+        
+//     }
+// )};
